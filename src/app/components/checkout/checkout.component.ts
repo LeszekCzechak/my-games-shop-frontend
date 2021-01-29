22c363f9de8391edad65acaec4f6aa2b9527e076
Leszek Czechak <leszekczechak@gmail.com>
2021-01-29 23:13:39 +0100

    added responsive states

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from '@angular/forms';
 import {MyGamesShopFormService} from '../../services/my-games-shop-form.service';
+import {Country} from '../../common/country';
+import {State} from '../../common/state';
 
 @Component({
   selector: 'app-checkout',
@@ -15,6 +17,11 @@ export class CheckoutComponent implements OnInit {
 
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+
+  countries: Country[] = [];
+
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
 
   constructor(private formBuilder: FormBuilder,
               private myGamesShopService: MyGamesShopFormService) {
@@ -31,12 +38,14 @@ export class CheckoutComponent implements OnInit {
         street: [''],
         city: [''],
         country: [''],
+        region: [''],
         zipCode: ['']
       }),
       billingAddress: this.formBuilder.group({
         street: [''],
         city: [''],
         country: [''],
+        region: [''],
         zipCode: ['']
       }),
       creditCard: this.formBuilder.group({
@@ -64,20 +73,33 @@ export class CheckoutComponent implements OnInit {
         this.creditCardYears = data;
       }
     );
+
+    this.myGamesShopService.getCountries().subscribe(
+      data => {
+        console.log('Retrieved countries: ' + JSON.stringify(data));
+        this.countries = data;
+      }
+    );
   }
 
   onSubmit(): void {
     console.log('Handling the submit button');
     console.log(this.checkoutFormGroup.get('customer').value);
     console.log('The email address is: ' + this.checkoutFormGroup.get('customer').value.email);
+    console.log('The shipping address country is: ' + this.checkoutFormGroup.get('shippingAddress').value.country.name);
+    console.log('The shipping address state is: ' + this.checkoutFormGroup.get('shippingAddress').value.state.name);
   }
 
   copyShippingAddressToBillingAddress(event): void {
     if (event.target.checked) {
       this.checkoutFormGroup.controls.billingAddress
         .setValue(this.checkoutFormGroup.controls.shippingAddress.value);
+
+      this.billingAddressStates = this.shippingAddressStates;
+
     } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
+      this.billingAddressStates = [];
     }
   }
 
@@ -97,6 +119,28 @@ export class CheckoutComponent implements OnInit {
     this.myGamesShopService.getCreditCardMonths(startMonth).subscribe(
       data => {
         this.creditCardMonths = data;
+      }
+    );
+  }
+
+  getStates(formGroupName: string): void {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+
+    const countryCode = formGroup.value.country.code;
+    const countryName = formGroup.value.country.name;
+
+    console.log(`${formGroupName} country code: ${countryCode}`);
+    console.log(`${formGroupName} country name: ${countryName}`);
+
+    this.myGamesShopService.getStates(countryCode).subscribe(
+      data => {
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        formGroup.get('state').setValue(data[0]);
       }
     );
   }
--- src/app/services/my-games-shop-form.service.ts
+++ src/app/services/my-games-shop-form.service.ts
@@ -1,12 +1,34 @@
 import {Injectable} from '@angular/core';
 import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {Country} from '../common/country';
+import {map} from 'rxjs/operators';
+import {State} from '../common/state';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class MyGamesShopFormService {
 
-  constructor() {
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountry>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    );
+  }
+
+  getStates(theCountryCode: string): Observable<State[]> {
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${theCountryCode}`;
+
+    return this.httpClient.get<GetResponseStates>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    );
   }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
@@ -32,4 +54,16 @@ export class MyGamesShopFormService {
     return of(data);
 
   }
+}
+
+interface GetResponseCountry {
+  _embedded: {
+    countries: Country[];
+  };
+}
+
+interface GetResponseStates {
+  _embedded: {
+    states: State[];
+  };
 }
